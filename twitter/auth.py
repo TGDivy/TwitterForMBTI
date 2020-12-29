87cec015240b9edbe9beed5b493b8a93c5991d47
Divy Bramhecha <s1885517@ed.ac.uk>
2020-12-29 10:48:13 +0530

    authenticating twiitter app

---
+++ twitter/auth.py
@@ -0,0 +1,18 @@
+import tweepy
+
+keys_dict = {
+        'consumer_key':        '7QBptQNO9Ya2XtDzjswaW27uR',
+        'consumer_secret':     'HOa8SsMhNgZVfucUUSUaGtlGOvft8goeIflTuAvXM8mCsF7vWK',
+        'access_token_key':    '1022541474238488576-Ap0fc5Y0ZFpmJXmd1OiqxsOkyfwm2M',
+        'access_token_secret': 'DDY31UUCQhvfr1UPIZ3RFD9qCPRfZJp2JOJyfhE78mQD5'
+    }
+
+def auth():
+
+    auth = tweepy.OAuthHandler(keys_dict['consumer_key'], keys_dict['consumer_secret'])
+    auth.set_access_token(keys_dict['access_token_key'], keys_dict['access_token_secret'])
+    api = tweepy.API(auth)
+    return keys_dict, api
+
+if __name__ == "__main__":
+    pass
