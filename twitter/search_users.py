32a0e672ff744eb21e13145a4dde908029299e8a
Divy Bramhecha <s1885517@ed.ac.uk>
2020-12-31 07:31:54 +0530

    creates two seperate files

--- twitter/search_users.py
+++ twitter/search_users.py
@@ -10,56 +10,75 @@ import tweepy
 class search:
     def __init__(self):
         self.twitter_keys, self.api = auth.auth()
+        
 
     def search_all(self, query):
+        self.saved = 0
         for i in tqdm(range(1, 52)):
             self.search(query, i)
+        print("total saved of ",query, self.saved)
 
     def search(self, query, page=None):
-        c = self.check(query, page)
+        c = self.check("#"+query, page)
         if c == 1:
-            results = self.api.search_users(query, page=page)
-            # utils.parse(results[0])
-            self.save_results(query, page, results)
+            try:
+                results = self.api.search_users("#"+query, page=page)
+                # utils.parse(results[0])
+                self.save_results(query, page, results)
+            except Exception as ex:
+                print("#######################")
+                print(ex)
+                print("#######################")
 
     def save_results(self, query, page, results, fresh=False):
         if fresh:
-            file_handle = open("users.json", "w")
+            json_file = open("users.json", "w")
+            json_file2 = open("users_likes.json", "w")
         else:
-            file_handle = open("users.json", "r")
+            json_file = open("users.json", "r")
+            json_file2 = open("users_likes.json", "r")
 
-        with file_handle as json_file:
-            data = json.load(json_file)
-            for result in results:
-                if result.id_str in data:
-                    continue
+        data = json.load(json_file)
+        data2 = json.load(json_file2)
+        for result in results:
+            if result.id_str in data:
+                continue
 
-                if self.profile_validity(query, result):
-                    dic = {
-                        result.id_str:
-                           {"screen_name":result.screen_name,
-                            "name":result.name,
-                            "bio":result.description,
-                            "MBTI type":"",
-                            "posts_liked":[],
-                            "posts_posted":[],
-                            "query":query,
-                            "page":page,
-                            "favorites_count:":result.favourites_count,
-                            "statuses_count":result.statuses_count,
-                            "location":result.location,
-                            "queried_at":datetime.today()}
-                    }
-                    data.update(dic)
+            if self.profile_validity(query, result):
+                dic = {
+                    result.id_str:
+                        {"screen_name":result.screen_name,
+                        "name":result.name,
+                        "bio":result.description,
+                        "MBTI type":"",
+                        # "posts_liked":[],
+                        # "posts_posted":[],
+                        "query":query,
+                        "type":query,
+                        "page":page,
+                        "favorites_count:":result.favourites_count,
+                        "statuses_count":result.statuses_count,
+                        "location":result.location,
+                        "queried_at":datetime.today()}
+                }
+                dic2 = {result.id_str: {"posts_liked":[], "posts_posted":[] }}
+                
+                data.update(dic)
+                data2.update(dic2)
+                self.saved+=1
+        json_file.close
+        json_file2.close
 
         with open("users.json", "w") as json_file:
             json.dump(data, json_file, indent=4, cls=utils.DateTimeEncoder)
+        with open("users_likes.json", "w") as json_file:
+            json.dump(data2, json_file, indent=4, cls=utils.DateTimeEncoder)
 
     def check(self, query, page):
         with open("users.json", "r") as json_file:
             data = json.load(json_file)
             for id in data.keys():
-                if data[id]["query"] == str(query) and data[id]["page"] == str(page):
+                if str(data[id]["query"]) == str(query) and str(data[id]["page"]) == str(page):
                     print("already queried")
                     return 0
         return 1
@@ -70,7 +89,6 @@ class search:
         display_name = result.screen_name.lower()
         description = result.description.lower()
         return query not in name and query not in display_name and query in description
-
 
 def main():
     s = search()
