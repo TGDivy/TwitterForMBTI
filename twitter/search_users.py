9e0608f5a45ad3661b3cb4fe6468591d61ffdf08
Divy Bramhecha <s1885517@ed.ac.uk>
2020-12-31 14:37:26 +0530

    adapat parameters

--- twitter/search_users.py
+++ twitter/search_users.py
@@ -3,20 +3,24 @@ import csv
 
 import utils
 import auth
+import parameters
+
 from datetime import datetime
 from tqdm import tqdm
 import tweepy
 
 class search:
     def __init__(self):
-        self.twitter_keys, self.api = auth.auth()
+        self.twitter_keys, self.api = auth.auth(0)
         
-
     def search_all(self, query):
         self.saved = 0
-        for i in tqdm(range(1, 52)):
+        for i in tqdm(range(1, parameters.MAX_PAGES_FOR_USERS+1)):
             self.search(query, i)
+            if self.saved >parameters.MAX_USERS_PER_TYPE:
+                break
         print("total saved of ",query, self.saved)
+        self.users_likes()
 
     def search(self, query, page=None):
         c = self.check("#"+query, page)
@@ -26,61 +30,46 @@ class search:
                 # utils.parse(results[0])
                 self.save_results(query, page, results)
             except Exception as ex:
-                print("#######################")
                 print(ex)
-                print("#######################")
-
-    def save_results(self, query, page, results, fresh=False):
-        if fresh:
-            json_file = open("users.json", "w")
-            json_file2 = open("users_likes.json", "w")
-        else:
-            json_file = open("users.json", "r")
-            json_file2 = open("users_likes.json", "r")
-
-        data = json.load(json_file)
-        data2 = json.load(json_file2)
+                
+    def save_results(self, query, page, results):
+        updater = utils.update_json("users.json")
         for result in results:
-            if result.id_str in data:
+            if result.id_str in updater.data:
                 continue
-
             if self.profile_validity(query, result):
                 dic = {
-                    result.id_str:
-                        {"screen_name":result.screen_name,
+                    result.id_str:{
+                        "screen_name":result.screen_name,
                         "name":result.name,
                         "bio":result.description,
                         "MBTI type":"",
-                        # "posts_liked":[],
-                        # "posts_posted":[],
                         "query":query,
-                        "type":query,
                         "page":page,
                         "favorites_count:":result.favourites_count,
                         "statuses_count":result.statuses_count,
                         "location":result.location,
-                        "queried_at":datetime.today()}
-                }
-                dic2 = {result.id_str: {"posts_liked":[], "posts_posted":[] }}
-                
-                data.update(dic)
-                data2.update(dic2)
+                        "queried_at":datetime.today()
+                        }
+                    }
+                updater.data.update(dic)
                 self.saved+=1
-        json_file.close
-        json_file2.close
+        updater.write()
 
-        with open("users.json", "w") as json_file:
-            json.dump(data, json_file, indent=4, cls=utils.DateTimeEncoder)
-        with open("users_likes.json", "w") as json_file:
-            json.dump(data2, json_file, indent=4, cls=utils.DateTimeEncoder)
+    def users_likes(self):
+        updater = utils.update_json("users_likes.json")
+        user_ids = json.load(open("data/users.json", "r")).keys()
+        for id in user_ids:
+            if id not in updater.data:
+                updater.data[id] = {"posts_liked":[], "posts_posted":[]}
+        updater.write()
 
     def check(self, query, page):
-        with open("users.json", "r") as json_file:
-            data = json.load(json_file)
-            for id in data.keys():
-                if str(data[id]["query"]) == str(query) and str(data[id]["page"]) == str(page):
-                    print("already queried")
-                    return 0
+        file = utils.update_json("users.json")
+        for id in file.data.keys():
+            if str(file.data[id]["query"]) == str(query) and str(file.data[id]["page"]) == str(page):
+                print("already queried")
+                return 0
         return 1
 
     def profile_validity(self, query, result):
@@ -88,31 +77,20 @@ class search:
         name = result.name.lower()
         display_name = result.screen_name.lower()
         description = result.description.lower()
-        return query not in name and query not in display_name and query in description
+
+        value = True
+        value = value and query not in name and query not in display_name
+        value = value and query in description
+        value = value and not result.protected
+
+        return value
 
 def main():
     s = search()
-    keywords = [
-        "INTJ",
-        "INTP",
-        "ENTP",
-        "ENTJ",
-        "INFJ",
-        "INFP",
-        "ENFP",
-        "ENFJ",
-        "ISTJ",
-        "ISFJ",
-        "ESTJ",
-        "ESFJ",
-        "ISTP",
-        "ISFP",
-        "ESTP",
-        "ESFP",
-    ]
-    for keyword in keywords:
-        s.search_all(keyword)
-
+    s.users_likes()
+    
+    # for keyword in parameters.KEYWORDS:
+    #     s.search_all(keyword)
 
 if __name__ == "__main__":
     main()
